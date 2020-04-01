5dad8041fdcadb4671a31bf3b40e3b39c39d8820
Lin, Ming Chih <mingchih.lin8@gmail.com>
2020-04-01 18:39:23 +0800

    Add example code to read range
    Read range is more efficient than read single cell one by one

--- readEXCEL/readEXCEL.py
+++ readEXCEL/readEXCEL.py
@@ -7,17 +7,31 @@ ex.Visible = False
 ex.DisplayAlerts = False
 
 #open EXCEL file
-wb = ex.Workbooks.Open('d:/demo/example.xlsx')
+workbook = ex.Workbooks.Open(r'D:\Documents\GitHub\HFSS-Automation\readEXCEL\example.xlsx')
 
 #get worksheet
-ws=wb.Worksheets[1]
+worksheet=workbook.Worksheets[1]
 
-#read cell Text
+#read single cell text
 total_score = 0
 for i in [1, 2, 3, 4]:
-    course=ws.Range['A'+str(i)].Text
-    score=ws.Range['B'+str(i)].Text
+    course = worksheet.Range['A'+str(i)].Text
+    score = worksheet.Range['B'+str(i)].Text
     total_score += float(score)
     AddWarningMessage('{}: {}'.format(course, score))
 
 AddWarningMessage('Total: {}'.format(total_score))
+
+#read range
+xlrange = worksheet.Range["A1", "B4"]
+AddWarningMessage(str(list(xlrange.Value2)))
+
+''' Shown in HFSS Message Manager
+*Global - Messages
+  [warning] Math: 80
+  [warning] Physics: 70
+  [warning] History: 60
+  [warning] Biology: 50
+  [warning] Total: 260.0
+  [warning] ['Math', 80.0, 'Physics', 70.0, 'History', 60.0, 'Biology', 50.0]
+'''
